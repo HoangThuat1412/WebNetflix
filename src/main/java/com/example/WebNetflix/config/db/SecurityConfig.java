86b1051a19c5930ffcf2971b8cd05729469d3b47
HoangThuat1412 <thuathvhd@gmail.com>
2025-03-13 16:54:26 +0700

    Spring Security cấu hình form Login

--- src/main/java/com/example/WebNetflix/config/db/SecurityConfig.java
+++ src/main/java/com/example/WebNetflix/config/db/SecurityConfig.java
@@ -4,29 +4,63 @@ package com.example.WebNetflix.config.db;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	
 	 @Bean
 	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		 http
 		 	.csrf(csrf -> csrf.disable())
 	        .authorizeHttpRequests((authz) -> authz
-	            .requestMatchers("/", "/login", "/css/**", "/js/**", "/home")
+	            .requestMatchers("/home", "/login", "/css/**", "/js/**", "/vendor/**")
 	            .permitAll().anyRequest()
 	            .authenticated()
 	            
 	        ).formLogin(formLogin -> formLogin
 	        		.loginPage("/login")
-	        		.defaultSuccessUrl("/", true)
 	        		.permitAll()
+	        		.defaultSuccessUrl("/login?sucess=true", true)
+	        		.failureUrl("/login?success=fail")
+	        		 .loginProcessingUrl("/j_spring_security_check")
 	        		);
 //	        .httpBasic(Customizer.withDefaults());
 			
 		 return  http.build();
 	    }
+	 
+	 @Bean
+	 public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+		 return new InMemoryUserDetailsManager(
+		            createUser("user", "123", "USER", passwordEncoder),
+		            createUser("admin", "admin123", "ADMIN" , passwordEncoder)
+		        );
+			}
+	 
+	 private UserDetails createUser(String username, String passWord, String role,  PasswordEncoder passwordEncoder) {
+	        return User.builder()
+	            .username(username)
+	            .password(passwordEncoder().encode(passWord)) // Mã hóa mật khẩu
+	            .roles(role)
+	            .build();
+	    }
+	 
+	 @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+	
+	 
 }
